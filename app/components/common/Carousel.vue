b1680ebbb6dee0551f72b410b758ac7b4b67fde7
rearcar92 <rearcar92@gmail.com>
2019-03-19 10:32:43 +0900

    carousel slot 적용

---
+++ app/components/common/Carousel.vue
@@ -0,0 +1,39 @@
+<template>
+  <figure id="visual">
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <slot name="link">
+        <slot name="image"></slot>
+    </slot>
+  </b-carousel>
+</figure>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <main-carousel></main-carousel>
+    <main-carousel>
+      <a slot="link" href="http://twinkring.com/">
+        <b-carousel-slide caption="" slot="image" img-src="http://twinkring.com/2019/02/star/star_fullbanner.jpg" />
+      </a>
+      <a slot="link" href="http://thechanran.com/">
+        <b-carousel-slide caption="" slot="image" img-src="http://twinkring.com/2019/03/bk/bk_fullbanner.jpg" />
+      </a>  
+    </main-carousel>
   </div>
 </template>
 
 <script>
-import mainCarousel from '~/components/main/Carousel.vue';
+import mainCarousel from '~/components/common/Carousel.vue';
 export default {
   components: {
     mainCarousel
